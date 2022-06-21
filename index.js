3e6ee49cc7e28b9fac8a05791932649d94780a61
Grttechie <kadiriayodele.ka@gmail.com>
2022-06-20 21:23:56 -0700

    Javascript code that prints my name, height, and country on the screen

---
+++ index.js
@@ -0,0 +1,9 @@
+//declaring variables and assigning values
+var my_name = "Ayodele Kadiri";
+var my_height = "174cm";
+var my_country = "Nigeria";
+
+//writing the variables into an html document
+document.write("<h2>" + my_name + "</h2>" + "<br>")
+document.write("<h2>" + my_height + "</h2>" + "<br>")
+document.write("<h2>" + my_country + "</h2>" + "<br>")
